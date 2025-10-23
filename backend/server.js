e4b951151f94c6641b678000e5b9c9b8d11bcac0
Farahoz <farahugaas0@gmail.com>
2025-10-23 06:53:40 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,17 @@ import { registerUser } from './controller/UserController.js';
 import userRouter from './routes/UserRoute.js';
 import cookieParser from 'cookie-parser';
 import cardsRoute from './routes/cardRoute.js'
-import cors from 'cors';
+import cors from 'cors'
+
+
 const app = express();
 const PORT = 8000
 
 app.use(cors({
-  origin: 'https://phishingapp-2iom.onrender.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
+  origin: 'https://phishing-h4is.onrender.com', // frontend origin
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // haddii aad isticmaalayso cookies / credentials
 }));
 
 app.use(express.json());
