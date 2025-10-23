abc09b92f579a1e1552d7a07e9d6a77e950bcc97
Farahoz <farahugaas0@gmail.com>
2025-10-23 18:52:39 +0300

    backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: [react(),
-    
-     tailwindcss()],
-     server :{
-    proxy:{
-      '/api': "https://phishingapp-2iom.onrender.com"
-    }
-  }
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://phishingapp-2iom.onrender.com', // backend URL sax ah
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
